25df3b8ff01bdadfe461d9f9dd0505c261804592
medericc <bleublanctour1@>
2026-01-04 04:19:39 +0100

    ee

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,10 @@ export default function Home() {
     const matchLinksByPlayer: Record<string, { name: string; url: string }[]> = {
         "L. JEROME": [ 
             // { name: "Schio", url: "https://www.fiba.basketball/en/events/api/game-live-info/128317/detail" },
-    
+    //  { name: "Landerneau", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2724068/bs.html" },
+       
+            { name: "Lattes Montpellier", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2724062/bs.html" },
+       
       { name: "Roche Vendée", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2724051/bs.html" },
        
             { name: "Basket Landes", url: "https://fibalivestats.dcd.shared.geniussports.com/u/FFBB/2724047/bs.html" },
